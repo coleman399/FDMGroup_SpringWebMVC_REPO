0553f895b326867ff4c807ca6fa47aec75a21dfe
dillon.coleman <coleman399@gmail.com>
2022-05-04 08:37:51 -0700

    caught project up to class

--- spring_web_mvc/src/main/java/com/fdmgroup/spring_web_mvc/controller/RegistrationController.java
+++ spring_web_mvc/src/main/java/com/fdmgroup/spring_web_mvc/controller/RegistrationController.java
@@ -2,13 +2,24 @@ package com.fdmgroup.spring_web_mvc.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @Autowired
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String goToRegisterPage() {
@@ -37,11 +48,28 @@ public class RegistrationController {
     // by adding the following verbiage to methods it is possible print webpages
     // <<<<<<<<< considered "old school"
     // HttpServletResponse response throws IOException
-    @RequestMapping(value = "/processCalculate", method = RequestMethod.GET)
-    public String goToProcessCalculate(HttpServletRequest request) {
-        int number1 = Integer.parseInt(request.getParameter("num1"));
-        int number2 = Integer.parseInt(request.getParameter("num2"));
-        int result = number1 + number2;
+    // @RequestMapping(value = "/processCalculate", method = RequestMethod.GET)
+    // public String goToProcessCalculate(HttpServletRequest request) {
+    //     int number1 = Integer.parseInt(request.getParameter("num1"));
+    //     int number2 = Integer.parseInt(request.getParameter("num2"));
+    //     int result = number1 + number2;
+
+    //     System.out.println("Result : " + result);
+    //     return "index";
+
+    //     // the syntax need to print the page
+
+    //     // PrintWriter out = response.getWriter();
+    //     // out.println("<html>");
+    //     // out.println("<head><title>Result Page</title></head>");
+    //     // out.println("<body><p>" + result + "</p></body>");
+    //     // out.println("</html>");
+
+    // }
+
+    @RequestMapping(value = "/processCalculate", method = RequestMethod.POST)
+    public String goToProcessCalculate(@RequestParam int num1, @RequestParam int num2) {
+        int result = num1 + num2;
 
         System.out.println("Result : " + result);
         return "index";
@@ -54,6 +82,14 @@ public class RegistrationController {
         // out.println("<body><p>" + result + "</p></body>");
         // out.println("</html>");
 
+    }
+
+    @GetMapping("/find/{lang}/{id}")
+    public String findBook(@PathVariable String lang, @PathVariable int id, Model model) {
+        System.out.println("Book info language is " + lang + " and the version id equals " + id);
+        model.addAttribute("language", lang);
+        model.addAttribute("version", id);
+        return "book-info";
     }
 
 }
---
+++ spring_web_mvc/src/main/java/com/fdmgroup/spring_web_mvc/controller/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.fdmgroup.spring_web_mvc.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public int calculate(int num1, int num2) {
+        return num1 + num2;
+    }
+}

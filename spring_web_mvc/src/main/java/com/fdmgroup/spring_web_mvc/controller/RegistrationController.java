e1715d32f3bae75a9b5d3da14b0763069f8b9c95
dillon.coleman <coleman399@gmail.com>
2022-05-03 19:35:53 -0700

    caught project up to class

--- spring_web_mvc/src/main/java/com/fdmgroup/spring_web_mvc/controller/RegistrationController.java
+++ spring_web_mvc/src/main/java/com/fdmgroup/spring_web_mvc/controller/RegistrationController.java
@@ -1,5 +1,13 @@
 package com.fdmgroup.spring_web_mvc.controller;
 
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +32,33 @@ public class RegistrationController {
         System.out.println("Processing 2...");
         return "index";
     }
-    
+
     // these annotations are equivalent
     // @RequestMapping(value = "/goToGoogle", method = RequestMethod.GET)
     @GetMapping("/goToGoogle")
     public String goToGoogle() {
         return "redirect:https://www.google.com";
+    }
+
+    // by adding the following verbiage to methods it is possible print webpages <<<<<<<<< considered "old school"
+    // HttpServletResponse response throws IOException 
+    @RequestMapping(value = "/processCalculate", method = RequestMethod.GET)
+    public String goToProcessCalculate(HttpServletRequest request) { 
+        int number1 = Integer.parseInt(request.getParameter("num1"));
+        int number2 = Integer.parseInt(request.getParameter("num2"));
+        int result = number1 + number2;
+
+        System.out.println("Result : " + result);
+        return "index";
+
+        // the syntax need to print the page
+
+        // PrintWriter out = response.getWriter();
+        // out.println("<html>");
+        // out.println("<head><title>Result Page</title></head>");
+        // out.println("<body><p>" + result + "</p></body>");
+        // out.println("</html>");
+
     }
 
 }
